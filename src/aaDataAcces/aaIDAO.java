b0a1aca06aa12b36fb9c11017500041986474a9d
Anthonio16 <anthonnyalmeida12@gmail.com>
2026-01-06 16:30:15 -0500

    Restructure project and add aaIDAO interface
    Moved project files out of the ROBOTIA subdirectory to the root structure for better organization. Added aaIDAO.java interface to src/aaDataAcces, defining basic CRUD operations for data access objects.

---
+++ src/aaDataAcces/aaIDAO.java
@@ -0,0 +1,12 @@
+
+
+import java.util.List;
+
+public interface aaIDAO<T> {
+    public T readBy(Integer id) throws Exception;
+    public boolean create(T entity) throws Exception;
+    public List<T> readAll() throws Exception;
+    public boolean update(T entity) throws Exception;
+    public boolean delete(Integer id) throws Exception;
+    //public Integer getMaxRow() throws Exception;
+}
